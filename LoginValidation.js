1da9223b9e7482532f3244cfb51c5ec7b7fc615c
Vasanth2415 <vasanthakumarpn@gmail.com>
2026-06-16 18:10:04 +0530

    first commit

---
+++ LoginValidation.js
@@ -0,0 +1,15 @@
+let validUsername = "Vasantha";
+let validPassword = "Mylogin123";
+
+
+let enteredUsername ="Vinoth";
+let enteredPassword ="Login123";
+
+
+if(enteredUsername === validUsername && enteredPassword === validPassword){
+console.log("Login is Successful");
+}
+
+else {
+    console.log("Login is Failure");
+}
